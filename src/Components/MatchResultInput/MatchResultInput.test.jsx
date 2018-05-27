755f5a8d1422e60d8691603a3ab5f0019d9cc14a
David Geo <dgeorgy@gmail.com>
2018-05-28 02:15:15 +1000

    Add validation tests for match result input

--- src/Components/MatchResultInput/MatchResultInput.test.jsx
+++ src/Components/MatchResultInput/MatchResultInput.test.jsx
@@ -5,6 +5,7 @@ import { ParticipantInput } from "../ParticipantInput";
 import { ParticipantList } from "../ParticipantList";
 import { WinnerInput } from "../WinnerInput";
 import { WinnerList } from "../WinnerList";
+import { ValidatedInput, ValidatedButton } from '../Validation';
 
 describe("when entering a match result", () => {
   let props;
@@ -30,6 +31,14 @@ describe("when entering a match result", () => {
     expect(grids.length).toBeGreaterThan(0);
   });
 
+//   describe("the rendered Grid", () => {
+//     it("should contain everything else that gets rendered", () => {
+//       const wrappingGrid = resultInput().find("Grid").first();
+
+//       expect(wrappingGrid.children()).toEqual(resultInput().children());
+//     });
+//   });
+
   it("should always renders a `ParticipantInput`", () => {
     expect(resultInput().find(ParticipantInput).length).toBe(1);
   });
@@ -45,4 +54,88 @@ describe("when entering a match result", () => {
   it("should always renders a `ParticipantList`", () => {
     expect(resultInput().find(WinnerList).length).toBe(1);
   });
+
+//   it('FirstTeamSize Input calls "updateFirstTeamSize()" on change', () => {
+//     const wrapper = resultInput();
+//     const event = {target: {name: "firstTeamSize", value: "1"}};
+//     const spy = jest.spyOn(wrapper.instance(), 'updateFirstTeamSize');
+//     wrapper.update();
+//     // resultInput().find("input[name='firstTeamSize']").simulate('change');
+//     wrapper.find("input").first().simulate('change', event);
+//     expect(spy).toHaveBeenCalled();
+//   });
+
+  it('1st Team Size textbox should update to inputted value on change', () => {
+    const event = {target: {name: "firstTeamSize", value: "2"}};
+    resultInput().find(ValidatedInput).at(0).prop('onChange')(event);
+    resultInput().update();
+
+    expect(resultInput().find(ValidatedInput).first().props().value).toBe("2");
+  });
+
+  it('2nd Team Size textbox should update to inputted value on change', () => {
+    const event = {target: {name: "secondTeamSize", value: "3"}};
+    resultInput().find(ValidatedInput).at(1).prop('onChange')(event);
+    resultInput().update();
+
+    expect(resultInput().find(ValidatedInput).at(1).props().value).toBe("3");
+  });
+
+  // Required validation
+  it('Save button should be disabled if all fields are empty', () => {
+    const saveBtn = resultInput().find(ValidatedButton).first().children().find('button').first();
+    expect(saveBtn.props().disabled).toBe(true);
+  });
+
+  it('Save button should be disabled if only 1st Team Size is entered', () => {
+    const event = {target: {name: "firstTeamSize", value: "2"}};
+    resultInput().find(ValidatedInput).at(0).prop('onChange')(event);
+    resultInput().update();
+
+    const saveBtn = resultInput().find(ValidatedButton).first().children().find('button').first();
+    expect(saveBtn.props().disabled).toBe(true);
+  });
+
+  it('Save button should be disabled if only 1st & 2nd Team Size is entered', () => {
+    let event = {target: {name: "firstTeamSize", value: "2"}};
+    resultInput().find(ValidatedInput).at(0).prop('onChange')(event);
+    event = {target: {name: "secondTeamSize", value: "3"}};
+    resultInput().find(ValidatedInput).at(1).prop('onChange')(event);
+    resultInput().update();
+
+    const saveBtn = resultInput().find(ValidatedButton).first().children().find('button').first();
+    expect(saveBtn.props().disabled).toBe(true);
+  });
+
+  it('Save button should be disabled if only 1st, 2nd Team Size and Participants are entered', () => {
+    let event = {target: {name: "firstTeamSize", value: "2"}};
+    resultInput().find(ValidatedInput).at(0).prop('onChange')(event);
+    event = {target: {name: "secondTeamSize", value: "3"}};
+    resultInput().find(ValidatedInput).at(1).prop('onChange')(event);
+    resultInput().instance().addParticipant('Steve');
+    resultInput().update();
+
+    const saveBtn = resultInput().find(ValidatedButton).first().children().find('button').first();
+    expect(saveBtn.props().disabled).toBe(true);
+  });
+
+  it('Save button should be enabled if all fields entered', () => {
+    let event = {target: {name: "firstTeamSize", value: "2"}};
+    resultInput().find(ValidatedInput).at(0).prop('onChange')(event);
+    event = {target: {name: "secondTeamSize", value: "3"}};
+    resultInput().find(ValidatedInput).at(1).prop('onChange')(event);
+    resultInput().instance().addParticipant('Steve');
+    resultInput().instance().addWinner('George');
+    resultInput().update();
+
+    const saveBtn = resultInput().find(ValidatedButton).first().children().find('button').first();
+    expect(saveBtn.props().disabled).toBe(false);
+  });
+
+  // Test participant input/list interaction
+
+  // Test node winRates calculator (potentially make it an npm package so its common)
+
+
+  
 });
